f546ecd41c569ad617e9b17d4f414754dcf116bb
Lorenzo <lorenzo.it@outlook.com>
2018-05-11 09:28:25 +0200

    modified:   Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
-FROM python:3.4-alpine
-ADD . /code
-WORKDIR /code
+FROM ubuntu:16.04
+MAINTAINER Lorenzo
+RUN apt-get update -y && \
+    apt-get install -y python-pip python-dev
+COPY ./requirements.txt /app/requirements.txt
+WORKDIR /app
 RUN pip install -r requirements.txt
-CMD ["python", "hello.py"]
+COPY . /app
+CMD [ "python", "hello.py"]
--- hello.py
+++ hello.py
@@ -13,22 +13,18 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-
 class NameForm(Form):
     valeur1 = FloatField('valeur 1 : ', validators=[InputRequired("Entrez une valeur")])
     valeur2 = FloatField('valeur 2 : ', validators=[InputRequired("Entrez une valeur")])
     submit = SubmitField('Calculer')
 
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -38,7 +34,6 @@ def index():
         session['result'] = value_format
         return redirect(url_for('index'))
     return render_template('index.html', form=form, result=session.get('result'))
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
